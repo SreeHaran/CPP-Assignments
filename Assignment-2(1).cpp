748b54b7089d0c8a84ceb2e675ce1ee8cab23b4f
Sree Haran <23sreeharan@gmail.com>
2020-07-20 20:46:21 +0530

    Add files via upload
    This the first program in Assignment-2 (OOPs class)

---
+++ Assignment-2(1).cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+class Student{
+    private:
+    string mName;
+    int mRollNo;
+
+    public:
+    void setDetails(string, int);
+    string getName(void);
+    int getRollNo(void);
+};
+void Student:: setDetails(string name, int rollNo){
+    mName = name;
+    mRollNo = rollNo;
+}
+string Student:: getName(void){
+    return mName;
+}
+int Student:: getRollNo(void){
+    return mRollNo;
+}
+int main()
+{
+    string name;
+    int rollNo;
+    Student stud1;
+    cin>> name; cin>> rollNo;
+    stud1.setDetails(name, rollNo);
+    cout << "Details:-\nName is: " << stud1.getName()<< endl << "Rollno. is: " << stud1.getRollNo();
+    return 0;
+}
